4069c91720575c5f3be3a12d93403b82c89cf59d
ekinnn35 <ekinn.9193@gmail.com>
2024-12-22 11:23:47 +0300

    Add project files.

---
+++ If Else Switch Case/Switch Case.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Rüya Manavına Hoş Geldiniz!");
+Console.WriteLine("Elma = 2 TL");
+Console.WriteLine("Armut = 3 TL");
+Console.WriteLine("Çilek = 2 TL");
+Console.WriteLine("Muz = 3 TL");
+Console.WriteLine("Diğer bütün meyveler = 4 TL");
+
+Console.WriteLine("\nHangi meyveyi satın almak istersiniz? (Elma/Armut/Çilek/Muz/Diğer): ");
+string secim = Console.ReadLine().ToLower(); // Küçük harfe dönüştürerek karşılaştırmayı kolaylaştırır
+
+int fiyat;
+
+switch (secim)
+{
+    case "elma":
+        fiyat = 2;
+        break;
+    case "armut":
+        fiyat = 3;
+        break;
+    case "çilek":
+        fiyat = 2;
+        break;
+    case "muz":
+        fiyat = 3;
+        break;
+    default:
+        fiyat = 4;
+        break;
+}
+
+Console.WriteLine($"Seçtiğiniz meyvenin fiyatı: {fiyat} TL");
---
+++ If Else/If Else.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Rüya Manavına Hoş Geldiniz!");
+Console.WriteLine("Elma = 2 TL");
+Console.WriteLine("Armut = 3 TL");
+Console.WriteLine("Çilek = 2 TL");
+Console.WriteLine("Muz = 3 TL");
+Console.WriteLine("Diğer bütün meyveler = 4 TL");
+
+Console.WriteLine("\nHangi meyveyi satın almak istersiniz? (Elma/Armut/Çilek/Muz/Diğer): ");
+string secim = Console.ReadLine().ToLower(); // Küçük harfe dönüştürerek karşılaştırmayı kolaylaştırır
+
+int fiyat;
+
+if (secim == "elma")
+{
+    fiyat = 2;
+}
+else if (secim == "armut")
+{
+    fiyat = 3;
+}
+else if (secim == "çilek")
+{
+    fiyat = 2;
+}
+else if (secim == "muz")
+{
+    fiyat = 3;
+}
+else
+{
+    fiyat = 4;
+}
+
+Console.WriteLine($"Seçtiğiniz meyvenin fiyatı: {fiyat} TL");
+
